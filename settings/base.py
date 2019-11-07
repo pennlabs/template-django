48c5ecd85e728e7654343d8b5551c9bc5b25199d
Armaan Tobaccowalla <13340433+ArmaanT@users.noreply.github.com>
2019-11-07 14:26:16 -0500

    Fix pennlabs URL

--- settings/base.py
+++ settings/base.py
@@ -15,8 +15,8 @@ import os
 import dj_database_url
 
 
-FRONTEND_DOMAIN = os.environ.get('FRONTEND_DOMAIN', 'pennlabs.com')
-BACKEND_DOMAIN = os.environ.get('BACKEND_DOMAIN', 'api.pennlabs.com')
+FRONTEND_DOMAIN = os.environ.get('FRONTEND_DOMAIN', 'pennlabs.org')
+BACKEND_DOMAIN = os.environ.get('BACKEND_DOMAIN', 'api.pennlabs.org')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
